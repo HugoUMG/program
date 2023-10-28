3647c3c57e7ad1d62c9b802c8cc9132204807e62
Hugo <umg@labqtz.local>
2023-10-28 07:45:23 -0600

    fist commit

---
+++ src/tarea1/Tarea1.java
@@ -0,0 +1,13 @@
+
+package tarea1;
+
+
+public class Tarea1 {
+
+
+    public static void main(String[] args) {
+    System.out.println("Hola Mundo");
+    System.out.println("Hola Mundo 2");    
+    }
+    
+}
